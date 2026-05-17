8fb69ef3cf63d3ae5d5bae85465b216cbfa54b05
Shadowed <12106248+Hanselljlh@users.noreply.github.com>
2026-05-17 06:05:15 +0300

    Add clipboard settings helper

---
+++ src/portable_crypt_recovery/core/clipboard.py
@@ -0,0 +1,8 @@
+"""Clipboard helpers.
+
+The GUI will use this later to auto-clear copied recovered passwords after 60 seconds.
+"""
+
+from __future__ import annotations
+
+DEFAULT_CLIPBOARD_CLEAR_SECONDS = 60
